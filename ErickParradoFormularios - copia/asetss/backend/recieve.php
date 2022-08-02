d712c6a32c33e349b4e9ce5e97efc892d4647e55
SantherEHPM <90868129+SantherEHPM@users.noreply.github.com>
2022-08-02 18:01:20 -0500

    Add files via upload

---
+++ ErickParradoFormularios - copia/asetss/backend/recieve.php
@@ -0,0 +1,5 @@
+<?php
+    //Declarar variable
+    $data =  $_POST;
+    var_export($data);
+?>
---
+++ ErickParradoFormularios - copia/asetss/scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+//crear contenedores
+
+const btnValidar=document.getElementById('validar');
+console.log(btnValidar);
+//btnValidar.addEventListener(dos parametros)
+btnValidar.addEventListener('click',function(a){
+    e.preventDefault();
+    let form=getElementById(dataF1);
+    console.log(form[1].value);
+});
